27d3025bd32a58b5cd2305527e1f4a719098f4fa
athulya2000 <athulyav00@gmail.com>
2022-11-28 23:37:40 +0530

    add completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewLivenewsComponent } from './view-livenews/view-livenews.component';
+import { AddNewsComponent } from './add-news/add-news.component';
+import { FormStyle } from '@angular/common';
+import { FormsModule } from '@angular/forms';
  
 const myRoute:Routes=[
   {
     path:"",
+    component:AddNewsComponent
+  },
+  {
+    path:"view",
     component:ViewLivenewsComponent
   }
 ]
@@ -16,12 +23,14 @@ const myRoute:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    ViewLivenewsComponent
+    ViewLivenewsComponent,
+    AddNewsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(myRoute)
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
